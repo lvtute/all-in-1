c6051958b24769778c848c7bfac63b7411bc0dce
Thuan <lvtute@gmail.com>
2021-07-31 23:19:10 +0700

    init commit

---
+++ src/test/java/com/thuan/allin1/Allin1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thuan.allin1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Allin1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
