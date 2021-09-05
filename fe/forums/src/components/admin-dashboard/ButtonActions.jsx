576235de0ec375a5fb48fffc9a757d6d02c31397
lvtute <lvtute@gmail.com>
2021-09-05 16:39:58 +0700

    complete reload table with recursion

--- fe/forums/src/components/admin-dashboard/ButtonActions.jsx
+++ fe/forums/src/components/admin-dashboard/ButtonActions.jsx
@@ -1,10 +1,11 @@
 import { Button, ButtonGroup } from "react-bootstrap";
 
-const genericDelete = (service) => (id) => {
+const genericDelete = (service, reloader) => (id) => {
   service
     .deleteById(id)
     .then((response) => {
       console.log(response);
+      reloader();
     })
     .catch((error) => {
       console.log("delete error:");
@@ -12,11 +13,11 @@ const genericDelete = (service) => (id) => {
     });
 };
 
-const ButtonActions = ({ service, id }) => {
+const ButtonActions = ({ service, id, reloader }) => {
   return (
     <ButtonGroup aria-label="Basic example">
       <Button variant="warning">Sửa</Button>
-      <Button variant="danger" onClick={() => genericDelete(service)(id)}>
+      <Button variant="danger" onClick={() => genericDelete(service, reloader)(id)}>
         Xóa
       </Button>
     </ButtonGroup>
--- fe/forums/src/components/admin-dashboard/user-manager/UserTable.jsx
+++ fe/forums/src/components/admin-dashboard/user-manager/UserTable.jsx
@@ -6,69 +6,82 @@ import userService from "../../../services/user.service";
 import queryParamUtils from "../../../utils/queryParamUtils";
 import { useHistory, useLocation } from "react-router-dom";
 
-const createActionButtons = (cell, row, rowIndex) => {
-  return <ButtonActions service={userService} id={row?.id} />;
-};
-
-const columns = [
-  {
-    dataField: "id",
-    text: "Id",
-  },
-  {
-    dataField: "username",
-    text: "Username",
-  },
-  {
-    dataField: "email",
-    text: "Email",
-  },
-  {
-    dataField: "fullName",
-    text: "Full name",
-  },
-  {
-    dataField: "facultyName",
-    text: "Faculty",
-  },
-  {
-    dataField: "roleNames",
-    text: "Role",
-  },
-  {
-    dataField: "actions",
-    text: "Actions",
-    formatter: createActionButtons,
-  },
-];
-
 const UserTable = () => {
+  const createActionButtons = (cell, row, rowIndex) => {
+    return (
+      <ButtonActions
+        service={userService}
+        id={row?.id}
+        reloader={() => reload(pageNum)}
+      />
+    );
+  };
+
+  const columns = [
+    {
+      dataField: "id",
+      text: "Id",
+    },
+    {
+      dataField: "username",
+      text: "Username",
+    },
+    {
+      dataField: "email",
+      text: "Email",
+    },
+    {
+      dataField: "fullName",
+      text: "Full name",
+    },
+    {
+      dataField: "facultyName",
+      text: "Faculty",
+    },
+    {
+      dataField: "roleNames",
+      text: "Role",
+    },
+    {
+      dataField: "actions",
+      text: "Actions",
+      formatter: createActionButtons,
+    },
+  ];
+
+  // get page num from the url query param
+  let pageNum = queryParamUtils.useQuery().get("page");
   let history = useHistory();
-  const query = queryParamUtils.useQuery();
 
   const path = useLocation().pathname;
-  // state to handle pageNum
 
   const [tableData, setTableData] = useState(Object);
 
-  const handleTableChange = (type, { page, sizePerPage }) => {
-    UserService.getAll(page, sizePerPage).then((response) => {
+  const reload = (pageNum, pageSize) => {
+    UserService.getAll(pageNum, pageSize).then((response) => {
       setTableData(response?.data);
+      // do recusion when return empty array
+      if (response.data.content.length === 0 && pageNum > 2) {
+        reload(pageNum - 1, pageSize);
+      }
     });
-
+    console.log(`reloaded with page = ${pageNum}`);
+  };
+  const handleTableChange = (type, { page, sizePerPage }) => {
+    reload(page, sizePerPage);
     // set the url param to match the current page
     history.replace({ pathname: path, search: `?page=${page}` });
   };
 
   useEffect(() => {
-    UserService.getAll(query.get("page"))
+    UserService.getAll(pageNum)
       .then((response) => {
         setTableData(response?.data);
       })
       .catch((error) => {
         console.log(error);
       });
-  }, [query]);
+  }, [pageNum]);
 
   return (
     <>
