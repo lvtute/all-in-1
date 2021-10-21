f405432469b56c5daeae17614b2fdfd1af9c1c06
lvtute <lvtute@gmail.com>
2021-10-21 19:28:03 +0700

    add topic

---
+++ be/src/main/java/com/hcmute/tlcn2021/model/Topic.java
@@ -0,0 +1,23 @@
+package com.hcmute.tlcn2021.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@Entity
+public class Topic {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @ManyToOne
+    @JoinColumn(name="faculty_id")
+    private Faculty faculty;
+
+}
