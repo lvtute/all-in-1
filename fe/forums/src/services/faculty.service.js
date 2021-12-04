f019bbcfd67b567a3570de98d15abdbe43f17865
HaDucDev <haductrung392000@gmail.com>
2021-12-04 20:44:29 +0700

    ok

--- fe/forums/src/services/faculty.service.js
+++ fe/forums/src/services/faculty.service.js
@@ -4,8 +4,24 @@ const getAll = () => {
   return http.get("/faculty");
 };
 
+
+const getById = (id = 0) => {
+  return http.get(`/faculty/${id}`);
+};
+
+const deleteById = (id = 0) => {
+  return http.delete(`/faculty/${id}`);
+};
+
+const update = (updateRequestObject) => {
+  return http.put("/faculty", updateRequestObject);
+};
+
 const facultyService = {
   getAll,
+  getById,
+  deleteById,
+  update,
 };
 
 export default facultyService;
